6e2c5b4b934c1eacdc06dda9baf76d9b2c3df522
Mariusz Jarocki <>
2026-03-07 12:03:08 +0100

    Obsługa danych

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MaterialModule } from '../material/material-module';
-import { JsonPipe } from '@angular/common';
 
 function notBlank(control: AbstractControl): ValidationErrors | null {
   return control.value?.trim().length > 0 ? null : { blank: true };
@@ -11,19 +10,20 @@ function notBlank(control: AbstractControl): ValidationErrors | null {
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule, MaterialModule, JsonPipe],
+  imports: [ReactiveFormsModule, MaterialModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home implements OnInit {
   form = new FormGroup({
-    firstname: new FormControl('', [Validators.required, notBlank]),
-    lastname: new FormControl('', [Validators.required, notBlank]),
+    firstname: new FormControl('', [notBlank]),
+    lastname: new FormControl('', [notBlank]),
   });
 
-  persons = [];
+  persons: any[] = [];
+  displayedColumns = ['id', 'firstname', 'lastname'];
 
-  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+  constructor(private http: HttpClient, private snackBar: MatSnackBar, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.loadData();
@@ -33,6 +33,7 @@ export class Home implements OnInit {
     this.http.get('/api/person').subscribe({
       next: (res) => {
         this.persons = res as any[];
+        this.cdr.markForCheck();
       },
       error: (e) => {
         this.snackBar.open(JSON.stringify(e.error), 'Zamknij', {
@@ -47,6 +48,7 @@ export class Home implements OnInit {
     this.http.post('/api/person', this.form.value).subscribe({
       next: (res) => {
         this.loadData();
+        this.form.reset();
         this.snackBar.open(JSON.stringify(res), 'Zamknij', {
           duration: 5000,
           panelClass: 'snackbar-success'
--- frontend/src/app/material/material-module.ts
+++ frontend/src/app/material/material-module.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -20,6 +21,7 @@ const MaterialComponents = [
   MatFormFieldModule,
   MatInputModule,
   MatSnackBarModule,
+  MatTableModule,
 ];
 
 @NgModule({
